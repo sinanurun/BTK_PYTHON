98cb8c426b0ada1b65f7870e119541dfeb8f175f
Sinan URUN <sinanurun24@gmail.com>
2023-08-31 21:00:33 +0300

    son

---
+++ gun04/ders16.py
@@ -0,0 +1,11 @@
+thisdict = {
+    "brand": "Ford",
+    "model": "Mustang",
+    "year": 1964
+}
+print(thisdict["brand"])
+print(thisdict.keys())
+print(thisdict.values())
+for baslik in thisdict:
+    print(baslik)
+    print(thisdict[baslik])
