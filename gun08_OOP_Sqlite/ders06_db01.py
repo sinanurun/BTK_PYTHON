0eba08680b26446c41725b4ac7979111d6b49551
Sinan URUN <sinanurun24@gmail.com>
2023-09-01 13:22:29 +0300

    ders06

--- gun08_OOP_Sqlite/ders06_db01.py
+++ gun08_OOP_Sqlite/ders06_db01.py
@@ -1,1 +1,16 @@
 import sqlite3 as sql
+
+vt = sql.connect('kitaplik.sqlite')
+imlec = vt.cursor()
+try:
+    imlec.execute('CREATE TABLE IF NOT EXISTS kitap_bilgisi (kitap_adi,kitap_yazari, okunma_durumu,begeni)')
+except:
+    print("tablo zaten vardı oluşturmadı")
+
+kitap_girisi = "INSERT INTO kitap_bilgisi VALUES ('Suç ve Ceza', 'Dostoyevski', 'hayır','*****')"
+
+imlec.execute(kitap_girisi)
+imlec.execute("INSERT INTO kitap_bilgisi VALUES ('Beyaz Diş', 'Jack LONDON', 'evet','***')")
+vt.commit()
+
+vt.close()
